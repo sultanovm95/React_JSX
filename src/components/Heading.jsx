33cb9b194872120d87bee727e5b48d38eb907fb1
Muhammadorif Sultanov <57433923+sultanovm95@users.noreply.github.com>
2020-12-28 01:58:47 -0800

    Adding Components Folder

---
+++ src/components/Heading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import List from "./List";
+
+function Heading() {
+  return (
+    <div>
+      <h1>Hello World! I am from another package!</h1>
+      <List></List>
+      <List></List>
+    </div>
+  );
+}
+
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // var ReactDOM = require("react-dom");
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.jsx";
+import App from "./components/App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
